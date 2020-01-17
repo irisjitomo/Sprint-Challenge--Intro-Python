56ccf37f0eac4a2686ee5e558b8ceed3f7dd27d6
unknown <irisjitomo@yahoo.com>
2020-01-17 11:06:45 -0500

    oop1 done

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,28 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+    # super().__init__(#VehicleAttributes)
+
+class FlightVehicle(Vehicle):
+    pass
+    # super().__init__(#VehicleAttributes)
+
+class Starship(Flightvehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass # new subclass attributes
+    # super().__init__(#GroundVehicleAttributes)
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+
